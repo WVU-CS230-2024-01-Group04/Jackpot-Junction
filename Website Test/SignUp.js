7972ef32b07ec2523c7e791a24b595d4e3d84e7c
lak00016 <156955751+Luke-Knight54@users.noreply.github.com>
2024-03-05 01:15:41 -0500

    SignUp
    Created backend for signup page

---
+++ Website Test/SignUp.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let signUpForm = document.getElementById("Registration");
+    signUpForm.addEventListener("submit", checkForm);
+
+    function checkForm(event) {
+        let newUsername = document.getElementById("newUsername");
+        let email = document.getElementById("email");
+        let password = document.getElementById("password");
+        let reEnterPassword = document.getElementById("reEnterPassword");
+
+        // Validation for re-entering the pass
+        if (password.value !== reEnterPassword.value) {
+            alert("Passwords do not match!");
+            event.preventDefault(); 
+        }
+        
+        if (password.length <= 8) {
+            alert("Password needs to be 8 or more characters!");
+            event.preventDefault();
+        }
+    
+        // If more validation is needed later add it here
+    }
+});
