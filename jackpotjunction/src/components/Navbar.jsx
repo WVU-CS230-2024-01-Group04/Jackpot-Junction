9e8a90e5f84786b905351c032abe46e21e5fd9ff
Dante-Capuzzi <dec00010@mix.wvu.edu>
2024-03-22 17:00:32 -0400

    More React Stuff
    Added in some organization for the style sheets, images, and components.

--- jackpotjunction/src/components/Navbar.jsx
+++ jackpotjunction/src/components/Navbar.jsx
@@ -2,21 +2,20 @@ import "bootstrap/dist/css/bootstrap.css"
 import React from "react"
 
 function Navbar()
-{
+{ 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
   
     <div className="navbar-nav" style={{marginLeft: '10px'}}>
-    <a className="navbar-brand" href="/">Jackpot Junction</a>
-      <a className="nav-item nav-link active" href="#">Home</a>
-      <a className="nav-item nav-link" href="#">About</a>
-      <a className="nav-item nav-link" href="#">Contact</a>
+      <h1 className="navbar-brand" >Jackpot Junction</h1>
     </div>
 
     <form className="form-inline" style={{ display: 'flex', justifyContent: 'flex-end', marginRight: '10px' }}>
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style={{marginRight: '10px'}}></input>
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+
+    
 
   </nav>
   )
--- jackpotjunction/src/pages/FrontPage.jsx
+++ jackpotjunction/src/pages/FrontPage.jsx
@@ -1,11 +1,18 @@
 import React from "react"
 import Navbar from "../components/Navbar"
+import "../styles/FrontPage.css"
+import logo from "../images/Jackpot_Junction_Logo.png"
 
 const FrontPage = () => {
     return(
-        <div>
+        <div className="FrontPage">
             <Navbar />
-            <div>FrontPage</div>
+            <img class="frontpage_logo" src={logo} alt="logo" width="800" height="500" />
+
+            <div class="frontpage_welcome">
+                <h1>Welcome to Jackpot Junction!</h1>
+                <h3>Please sign-in with your information and you'll be gambling in no time!</h3>
+            </div>
         </div>
     )
 }
