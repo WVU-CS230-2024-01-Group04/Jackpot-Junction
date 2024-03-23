bfc2ee3b30975ea6f1feae14f00fee51e5ddcb0a
kian-mcchesney <kianmcchesney@gmail.com>
2024-03-23 10:32:19 -0400

    add basic signup form

--- jackpotjunction/src/pages/SignUp.jsx
+++ jackpotjunction/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "../components/Navbar"
 import { Link } from "react-router-dom"
+import SignUpForm from "../components/SignUpForm"
 
 const SignUp = () => {
     return(
@@ -9,6 +10,7 @@ const SignUp = () => {
 
             <h1>Signup Page</h1>
             <p>Input your mothers maiden name NOW!</p>
+            <SignUpForm/>
             <Link to="/main">Main</Link>
         </div>
     )
