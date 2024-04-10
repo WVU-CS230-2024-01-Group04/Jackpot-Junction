e15b3fb15b35743bab4a47aedb7062b5251addb4
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-04-09 22:20:19 -0400

    Add files via upload

---
+++ SlotsPage.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+import slotsimg1 from "../images/slots.png"
+import GameCard from "../components/GameCard"
+
+const SlotsPage = () => {
+    return(
+        <div>
+            <Navbar currentPage="slots"/>
+
+            <h1>Slots Page</h1>
+            <h3>Here is where all slots-type games on the site are.</h3>
+
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', padding: '25px'}}>
+                <GameCard image={slotsimg1} link="/SlotsRules" title="Slots 1" description="The first slots game ever in existence its crazy."/>
+            </div>
+        </div>
+    )
+}
+
+export default SlotsPage
