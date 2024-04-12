045a6d8b7b331c32d0919fe8be000a03bd88e184
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-04-12 16:59:44 -0400

    Update SlotsRules.jsx

--- client/src/pages/SlotsRules.jsx
+++ client/src/pages/SlotsRules.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "../components/Navbar"
 import slotsimg1 from "../images/slots.png"
 import GameCard from "../components/GameCard"
+import "../styles/MainPage.css"
 
 const SlotsRules = () => {
     return(
@@ -18,7 +19,7 @@ const SlotsRules = () => {
                     You can bet a certain amount of money and watch the wheels spin.
                     </li>
                     <li>
-                    If central row has certain combinations, you win.
+                    If the wheels land in a certain combination, you win.
                     </li>
                     <li>Simple, Right?</li>
                 </ul>
@@ -27,11 +28,318 @@ const SlotsRules = () => {
             <div>
                 <h3>Here are the payouts:</h3>
                 
-                <p>placeholder</p>
+                <table>
+                    <tr>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji">🍌</td>
+                                <td>3</td>
+                                <td>50 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍌</td>
+                                <td>4</td>
+                                <td>100 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍌</td>
+                                <td>5</td>
+                                <td>150 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍌</td>
+                                <td>6</td>
+                                <td>200 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍌</td>
+                                <td>7</td>
+                                <td>250 points</td>
+                            </tr>
+                        </th>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji">🍎</td>
+                                <td>3</td>
+                                <td>10 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍎</td>
+                                <td>4</td>
+                                <td>20 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍎</td>
+                                <td>5</td>
+                                <td>30 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍎</td>
+                                <td>6</td>
+                                <td>40 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍎</td>
+                                <td>7</td>
+                                <td>50 points</td>
+                            </tr>
+                        </th>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji">🍒</td>
+                                <td>3</td>
+                                <td>100 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍒</td>
+                                <td>4</td>
+                                <td>200 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍒</td>
+                                <td>5</td>
+                                <td>300 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍒</td>
+                                <td>6</td>
+                                <td>400 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍒</td>
+                                <td>7</td>
+                                <td>500 points</td>
+                            </tr>
+                        </th>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji">🎰</td>
+                                <td>3</td>
+                                <td>500 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🎰</td>
+                                <td>4</td>
+                                <td>1000 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🎰</td>
+                                <td>5</td>
+                                <td>1500 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🎰</td>
+                                <td>6</td>
+                                <td>2000 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🎰</td>
+                                <td>7</td>
+                                <td>2500 points</td>
+                            </tr>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji">🍏</td>
+                                <td>3</td>
+                                <td>10 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍏</td>
+                                <td>4</td>
+                                <td>20 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍏</td>
+                                <td>5</td>
+                                <td>30 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍏</td>
+                                <td>6</td>
+                                <td>40 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍏</td>
+                                <td>7</td>
+                                <td>50 points</td>
+                            </tr>
+                        </th>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji">🍍</td>
+                                <td>3</td>
+                                <td>100 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍍</td>
+                                <td>4</td>
+                                <td>150 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍍</td>
+                                <td>5</td>
+                                <td>200 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍍</td>
+                                <td>6</td>
+                                <td>250 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍍</td>
+                                <td>7</td>
+                                <td>300 points</td>
+                            </tr>
+                        </th>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji">🍊</td>
+                                <td>3</td>
+                                <td>100 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍊</td>
+                                <td>4</td>
+                                <td>200 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍊</td>
+                                <td>5</td>
+                                <td>300 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍊</td>
+                                <td>6</td>
+                                <td>400 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍊</td>
+                                <td>7</td>
+                                <td>500 points</td>
+                            </tr>
+                        </th>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji" rowSpan={5}>🍋</td>
+                                <td>3</td>
+                                <td rowSpan={5}>0 Points</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                            </tr>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji" rowSpan={5}>🍉</td>
+                                <td>3</td>
+                                <td rowSpan={5}>0 Points</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                            </tr>
+                        </th>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji" rowSpan={5}>🍇</td>
+                                <td>3</td>
+                                <td rowSpan={5}>0 Points</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                            </tr>
+                        </th>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji">🍓</td>
+                                <td>3</td>
+                                <td>75 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍓</td>
+                                <td>4</td>
+                                <td>100 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍓</td>
+                                <td>5</td>
+                                <td>125 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍓</td>
+                                <td>6</td>
+                                <td>150 points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">🍓</td>
+                                <td>7</td>
+                                <td>175 points</td>
+                            </tr>
+                        </th>
+                        <th id = "topRow">
+                            <tr>
+                                <td className="Emoji">💰</td>
+                                <td>3</td>
+                                <td>200 Points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">💰</td>
+                                <td>4</td>
+                                <td>400 Points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">💰</td>
+                                <td>5</td>
+                                <td>600 Points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">💰</td>
+                                <td>6</td>
+                                <td>800 Points</td>
+                            </tr>
+                            <tr>
+                                <td className="Emoji">💰</td>
+                                <td>7</td>
+                                <td>1000 Points</td>
+                            </tr>
+                        </th>
+                    </tr>
+                </table>
             </div>
             
             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', padding: '25px'}}>
-                <GameCard image={slotsimg1} link="/slots1" title="Slots" />
+                <GameCard image={slotsimg1} link="/slots1" title="Slots"/>
             </div>
         </div>
     )
