2ccd1f24b6cd71fae0ee3ed51b5f2dd5533cc2e4
kian-mcchesney <kianmcchesney@gmail.com>
2024-04-23 18:36:33 -0400

    add terms of services, rerouted stuff, display username

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,29 +1,65 @@
-import React from "react"
-import Navbar from "../components/Navbar"
-import { Link } from "react-router-dom"
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
-
-
+import backgroundImage from "../images/backgroundImage.jpg"; 
+import MainPage from "./MainPage"; 
 
 const SignUp = () => {
-    return(
+    const navigate = useNavigate();
+    const [agreedToTerms, setAgreedToTerms] = useState(false);
+
+    const handleAgreeToTerms = () => {
+        setAgreedToTerms(true);
+    }
+
+    const handleSignIn = () => {
+        navigate("/main");
+    }
+
+    return (
         <div>
             <Navbar currentPage="login/signup"/>
-
             <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
-            <Link to="/main">Main</Link>
+                {({ user }) => (
+                    <React.Fragment>
+                        {!user && !agreedToTerms && (
+                            <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh' }}>
+                                <div className="container mt-5">
+                                    <div className="row justify-content-center">
+                                        <div className="col-md-6 text-center mb-4">
+                                            <img src={require("../images/vegas.png")} className="img-fluid icon" style={{ width: '900px', height: 'auto' }} alt="Icon" />
+                                        </div>
+                                        <div className="col-md-6" style={{ backgroundColor: '#f4f4f4', padding: '30px', borderRadius: '20px' }}> 
+                                            <form onSubmit={handleAgreeToTerms}>
+                                                <h1>Terms of Service</h1>
+                                                <p>Please read and agree to the terms of service before signing up.</p>
+                                                <ul style={{ listStyle: 'inside', paddingLeft: '20px' }}>
+                                                    <li>Rule 1</li>
+                                                    <li>Rule 2</li>
+                                                    <li>Rule 3</li>
+                                                </ul>
+                                                <label>
+                                                    <input type="checkbox" required /> I agree to the terms of service
+                                                </label>
+                                                <div></div>
+                                                <button type="submit" className="btn btn-primary">Agree and Continue</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                        {user && (
+                            <MainPage user={user} /> 
+                        )}
+                        {user && handleSignIn()}
+                    </React.Fragment>
+                )}
+            </Authenticator>
         </div>
-    )
+    );
 }
 
 export default SignUp;
--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import kermitImage from "../images/kermit.webp";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+
 
 const StatsPage = () => {
 
@@ -30,57 +32,68 @@ const StatsPage = () => {
     };
 
     return (
-        <div>
-            <Navbar currentPage="userpage" />
+        <Authenticator>
+ {({ signOut,user }) => (
+                    
 
-            <div className="container mt-4">
-                <div className="row">
-                    <div className="col-md-4">
-                        <div className="card text-center">
-                            <div className="card-body">
-                                <img src={kermitImage} alt="Profile" className="img-fluid rounded-circle mb-3" style={{ width: '150px' }} />
-                                <h4>Username: Kermit</h4>
-                                <button className="btn btn-primary">Report User</button>
-                                <button className="btn btn-primary">Edit Profile</button>
-                                <button className="btn btn-primary">Sign Out</button>
-                            </div>
+        <div>
+        <Navbar currentPage="userpage" />
+
+        <div className="container mt-4">
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="card text-center">
+                        <div className="card-body">
+                            <img src={kermitImage} alt="Profile" className="img-fluid rounded-circle mb-3" style={{ width: '150px' }} />
+                            <h4>Username: {user.username}</h4>
+                            <button className="btn btn-primary">Report User</button>
+                            <button className="btn btn-primary">Edit Profile</button>
+                            <button onClick={signOut}>Sign out</button>
                         </div>
                     </div>
-                    <div className="col-md-8">
-                        <h1>Stats</h1>
+                </div>
+                <div className="col-md-8">
+                    <h1>Stats</h1>
 
-                        <div className="card mb-3">
-                            <div className="card-body">
-                                <h3>Slots</h3>
-                                <p>Games Played: {slotsStats.gamesPlayed}</p>
-                                <p>Wins: {slotsStats.wins}</p>
-                                <p>Losses: {slotsStats.losses}</p>
-                                <p>Total Bet Amount: ${slotsStats.totalBetAmount}</p>
-                                <p>Total Winnings: ${slotsStats.totalWinnings}</p>
-                                <p>Highest Win: ${slotsStats.highestWin}</p>
-                                <p>Average Payout: {slotsStats.averagePayout}</p>
-                            </div>
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <h3>Slots</h3>
+                            <p>Games Played: {slotsStats.gamesPlayed}</p>
+                            <p>Wins: {slotsStats.wins}</p>
+                            <p>Losses: {slotsStats.losses}</p>
+                            <p>Total Bet Amount: ${slotsStats.totalBetAmount}</p>
+                            <p>Total Winnings: ${slotsStats.totalWinnings}</p>
+                            <p>Highest Win: ${slotsStats.highestWin}</p>
+                            <p>Average Payout: {slotsStats.averagePayout}</p>
                         </div>
+                    </div>
 
-                        <div className="card">
-                            <div className="card-body">
-                                <h3>Blackjack</h3>
-                                <p>Games Played: {blackjackStats.gamesPlayed}</p>
-                                <p>Wins: {blackjackStats.wins}</p>
-                                <p>Losses: {blackjackStats.losses}</p>
-                                <p>Total Bet Amount: ${blackjackStats.totalBetAmount}</p>
-                                <p>Total Winnings: ${blackjackStats.totalWinnings}</p>
-                                <p>Total Pushes: {blackjackStats.totalPushes}</p>
-                                <p>Total Busts: {blackjackStats.totalBusts}</p>
-                                <p>Winning Streaks: {blackjackStats.winningStreaks}</p>
-                                <p>Losing Streaks: {blackjackStats.losingStreaks}</p>
-                                <p>Average Win Rate: {blackjackStats.averageWinRate}%</p>
-                            </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3>Blackjack</h3>
+                            <p>Games Played: {blackjackStats.gamesPlayed}</p>
+                            <p>Wins: {blackjackStats.wins}</p>
+                            <p>Losses: {blackjackStats.losses}</p>
+                            <p>Total Bet Amount: ${blackjackStats.totalBetAmount}</p>
+                            <p>Total Winnings: ${blackjackStats.totalWinnings}</p>
+                            <p>Total Pushes: {blackjackStats.totalPushes}</p>
+                            <p>Total Busts: {blackjackStats.totalBusts}</p>
+                            <p>Winning Streaks: {blackjackStats.winningStreaks}</p>
+                            <p>Losing Streaks: {blackjackStats.losingStreaks}</p>
+                            <p>Average Win Rate: {blackjackStats.averageWinRate}%</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+                )}
+
+
+
+
+        </Authenticator>
+
     );
 };
 
